a730fb6ee684d9729b8471b2b3d2a90bfb5c9534
Hideaki Kanehara <sss_ftr.hid@icloud.com>
2018-07-30 18:05:34 +0900

    add aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,31 +1,29 @@
 alias sudo="sudo -E"
 alias v="vim"
 alias n="nvim"
-alias gv="gvim"
 # git
 alias g="git"
-# cd
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-# rm, cp, mv
-alias rm="rm -i"
-alias cp="cp -vi"
-alias mv="mv -vi"
+alias gs="git status -sb"
+alias gd="git diff"
 # ls
 alias ll="ls -lhtr"
 alias ll.="ls -ldhtr .*"
 alias lsd="ls -F | \grep / | tr '\n' '\t'"
 alias lsf="ls -F | \grep -v / | tr '\n' '\t'"
-# mkdir
+# cd
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+# other builtin commands
+alias rm="rm -i"
+alias cp="cp -vi"
+alias mv="mv -vi"
+alias ln="ln -v"
 alias mkdir="mkdir -p"
-# grep
 alias grep="grep -in --color"
 # ssh
 alias sshj="ssh -L 9000:127.1:9000"
 alias sshvnc="ssh -L 5900:127.1:5900"
-# git
-alias gs="git status -sb"
 # docker
 alias d="docker"
 alias dc="docker-compose"
