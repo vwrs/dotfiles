106fc4940a0cbbae89a1638b0095da489211f7f9
Hideaki Kanehara <hideaki.kanehara@gmail.com>
2018-08-23 11:03:16 +0900

    minor updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,20 +1,13 @@
+# builtin commands
+# ----------
 alias sudo="sudo -E"
-alias v="vim"
-alias n="nvim"
-# git
-alias g="git"
-alias gs="git status -sb"
-alias gd="git diff"
-# ls
 alias ll="ls -lhtr"
 alias ll.="ls -ldhtr .*"
 alias lsd="ls -F | \grep / | tr '\n' '\t'"
 alias lsf="ls -F | \grep -v / | tr '\n' '\t'"
-# cd
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
-# other builtin commands
 alias rm="rm -i"
 alias cp="cp -vi"
 alias mv="mv -vi"
@@ -24,11 +17,41 @@ alias grep="grep -in --color"
 # ssh
 alias sshj="ssh -L 9000:127.1:9000"
 alias sshvnc="ssh -L 5900:127.1:5900"
-# docker
-alias d="docker"
-alias dc="docker-compose"
+
+# non-builtin commands
+# ----------
+if [ -x "`which vim`" ]; then
+  alias v="vim"
+fi
+if [ -x "`which nvim`" ]; then
+  alias n="nvim"
+fi
+if [ -x "`which git`" ]; then
+  alias g="git"
+  alias gs="git status -sb"
+  alias gd="git diff"
+fi
+if [ -x "`which docker`" ]; then
+  alias d="docker"
+  alias dc="docker-compose"
+fi
+if [ -x "`which tree`" ]; then
+  alias tree="tree -aC -I '.git|.DS_Store'"
+fi
+if [ -x "`which tmux`" ]; then
+  alias ta="tmux a"
+  alias tl="tmux ls"
+  alias tks="tmux kill-server"
+fi
+if [ -x "`which lualatex`" ]; then
+  alias lualatex="lualatex --file-line-error --synctex=1"
+fi
+if [ -x "`which luajittex`" ]; then
+  alias luajitlatex="luajittex --fmt=luajitlatex.fmt"
+fi
+
 # programming languages
-# --------------
+# ----------
 # php
 alias phpi="php -a"
 # rails
--- zsh/commands.zsh
+++ zsh/commands.zsh
@@ -1,6 +1,3 @@
-if [ -x "`which tree`" ]; then
-  alias tree="tree -aC -I '.git|.DS_Store'"
-fi
 if [ -d ~/.fzf ]; then
   [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 fi
@@ -9,11 +6,6 @@ if [ -x "`which rg`" ]; then
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
 
-if [ -x "`which tmux`" ]; then
-  alias ta="tmux a"
-  alias tl="tmux ls"
-  alias tks="tmux kill-server"
-fi
 if [ -x "`which convert`" ]; then
   img2pdf () {
     for f in *.(png|jpg|eps); do
@@ -26,11 +18,5 @@ if [ -d ~/.iterm2 ]; then
   alias it2dl="~/.iterm2/it2dl"
   alias imgcat="~/.iterm2/imgcat"
   alias imgls="~/.iterm2/imgls"
-fi
-if [ -x "`which lualatex`" ]; then
-  alias lualatex="lualatex --file-line-error --synctex=1"
-fi
-if [ -x "`which luajittex`" ]; then
-  alias luajitlatex="luajittex --fmt=luajitlatex.fmt"
 fi
 
