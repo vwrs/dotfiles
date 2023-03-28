da05ed850b866ead7cc5b12d6d45a26764591b91
Hideaki Kanehara <hideaki_kanehara@r.recruit.co.jp>
2023-03-28 17:35:52 +0900

    neovim: disable diagnostic for .env files

--- vim/user/plugins/lsp.lua
+++ vim/user/plugins/lsp.lua
@@ -233,3 +233,12 @@ null_ls.setup({
 })
 
 require('trouble').setup {}
+
+-- disable diagnostic for .env file
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*.env",
+  group = vim.api.nvim_create_augroup("__env", {clear=true}),
+  callback = function(args)
+    vim.diagnostic.disable(args.buf)
+  end
+})
