2a06ed8463ef1eb714ab951abc2b1e5f4664a912
Hideaki Kanehara <hideaki_kanehara@r.recruit.co.jp>
2023-10-06 18:38:55 +0900

    fix: nvim-treesitter: Parser not available for language "help"

--- vim/user/plugins/treesitter.lua
+++ vim/user/plugins/treesitter.lua
@@ -11,7 +11,7 @@ require('nvim-treesitter.configs').setup {
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
-  ignore_install = { },
+  ignore_install = { "help" },
 
   highlight = {
     enable = true,
